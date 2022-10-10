4d91dd2d49555365c102eaf906db2794b080ad1d
phlc <pedrocarvalho@outlook.com>
2022-10-09 22:05:47 -0300

    database name changed

--- backend/__init__.py
+++ backend/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from os import path
 
 db = SQLAlchemy()
-DB_NAME = "ignore_database.db"
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
