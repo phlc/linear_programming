6cafbd12796827f005c283e5e01107f8f3cb407e
phlc <pedrocarvalho@outlook.com>
2022-10-10 11:14:21 -0300

    main.py inside backend

--- backend/models.py
+++ backend/models.py
@@ -1,22 +1,28 @@
-from enum import unique
 from . import db 
 from sqlalchemy.sql import func
 
 
-recipe_ingredient = db.Table('recipe_ingredient',
-    db.Column('recipe_id', db.Integer, db.ForeignKey('recipe.id')),
-    db.Column('ingredient_id', db.Integer, db.ForeignKey('ingredient.id'))
-)
 
+class Association(db.Model):
+    __tablename__ = "association"
+    left_id = db.Column(db.ForeignKey("left.id"), primary_key=True)
+    right_id = db.Column(db.ForeignKey("right.id"), primary_key=True)
+    quantity = db.Column('quantity', db.Integer)
+    ingredient = db.relationship("Ingredient", back_populates="recipes")
+    recipe = db.relationship("Recipe", back_populates="ingredients")
 
 
 class Recipe(db.Model):
+    __tablename__ = "left"
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), unique=True)
-    prepare = db.Column(db.Text)
-    ingredients = db.relationship('Ingredient', secondary=recipe_ingredient, backref='ingredients')
+    howto = db.Column(db.Text)
+    ingredients = db.relationship('Association', back_populates='recipes')
 
 class Ingredient(db.Model):
+    __tablename__ = "right"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(150), unique=True)
-    unit = db.Column(db.String(10))
+    unit = db.Column(db.String(20))
+    recipes = db.relationship('Association', back_populates='ingredients')
+
