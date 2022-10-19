bf782000333b6b2183c91aa2f6589c077eb3fba2
Ana Laura Fernandes <analaura.fernandes@dtidigital.com.br>
2022-10-18 22:21:07 -0300

    Continuação da implementação da tela principal - Passo 1

---
+++ frontend/src/components/firstStep/firstStep.tsx
@@ -0,0 +1,86 @@
+import React, { Dispatch, SetStateAction } from "react";
+import {
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  TextField,
+} from "@mui/material";
+
+interface FirstStepProps {
+  ingredient: string;
+  handleIngredientChange: (event: SelectChangeEvent) => void;
+  setIngredientsList: Dispatch<SetStateAction<never[]>>;
+  ingredientsList: string[];
+}
+
+export default function FirstStep({
+  ingredient,
+  handleIngredientChange,
+  ingredientsList,
+  setIngredientsList,
+}: FirstStepProps) {
+  return (
+    <section className="flex flex-col w-1/3">
+      <h2 className="text-heading-semibold-5 text-blue-100 mb-2">Passo 1:</h2>
+      <div className="flex flex-col bg-gray-100 w-3/4 py-10 px-8 min-h-72 rounded-2xl">
+        <span className="text-body-semibold-1 mb-3">
+          Selecione o ingrediente
+        </span>
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Ex: Leite</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={ingredient}
+            label="Ex: Leite"
+            onChange={handleIngredientChange}
+            style={{ backgroundColor: "white" }}
+          >
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl>
+        <span className="text-body-semibold-1 my-3">Quantidade</span>
+        <TextField
+          id="outlined-basic"
+          label="Ex: 12L"
+          variant="outlined"
+          style={{ backgroundColor: "white" }}
+        />
+        <Button
+          variant="contained"
+          style={{
+            backgroundColor: "#E0FFF0",
+            width: 107,
+            alignSelf: "flex-end",
+            color: "#242121",
+            textTransform: "none",
+            fontFamily: "Montserrat",
+            fontWeight: 600,
+            marginTop: 20,
+          }}
+        >
+          Adicionar
+        </Button>
+      </div>
+      <div className="flex flex-col bg-gray-100 w-3/4 py-10 px-8 max-h-72 rounded-2xl mt-5">
+        <div className="flex flex-col bg-white shadow-1 rounded-xl max-w-sm py-10 px-8 max-h-72 rounded-2xl">
+          <span className="text-body-semibold-1 mb-3">
+            Lista de ingredientes adicionados:
+          </span>
+          {ingredientsList.map((item) => {
+            return (
+              <span id={item} className="text-body-regular-2">
+                {item}
+              </span>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
+}
--- frontend/src/landingPage/index.tsx
+++ frontend/src/landingPage/index.tsx
@@ -7,59 +7,36 @@ import {
   TextField,
 } from "@mui/material";
 import React, { useState } from "react";
+import FirstStep from "../components/firstStep/firstStep";
 
 export default function LandingPage() {
   const [ingredient, setIngredient] = useState("");
-  const handleChange = (event: SelectChangeEvent) => {
+  const [ingredientsList, setIngredientsList] = useState([]);
+
+  const handleIngredientChange = (event: SelectChangeEvent) => {
     setIngredient(event.target.value);
   };
 
   return (
     <div className="flex bg-blue-200 min-h-screen p-12">
-      <div className="flex flex-col bg-white shadow-1 rounded-3xl min-w-full p-6">
+      <div className="flex flex-col bg-white shadow-1 rounded-3xl min-w-full p-10">
         <h1 className="text-heading-semibold-4 mb-6">
           Otimização de Alimentos
         </h1>
         <div className="flex flex-row min-w-full  justify-between">
-          <section className="flex flex-col w-96">
-            <h2 className="text-heading-semibold-5 text-blue-100 mb-2">
-              Passo 1:
-            </h2>
-            <div className="flex flex-col bg-gray-100 max-w-sm py-10 px-8 max-h-72 rounded-2xl">
-              <span className="text-body-semibold-1 mb-3">
-                Selecione o ingrediente
-              </span>
-              <FormControl fullWidth>
-                <InputLabel id="demo-simple-select-label">Ex: Leite</InputLabel>
-                <Select
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
-                  value={ingredient}
-                  label="Ex: Leite"
-                  onChange={handleChange}
-                  style={{ backgroundColor: "white" }}
-                >
-                  <MenuItem value={10}>Ten</MenuItem>
-                  <MenuItem value={20}>Twenty</MenuItem>
-                  <MenuItem value={30}>Thirty</MenuItem>
-                </Select>
-              </FormControl>
-              <span className="text-body-semibold-1 my-3">Quantidade</span>
-              <TextField
-                id="outlined-basic"
-                label="Ex: 12L"
-                variant="outlined"
-                style={{ backgroundColor: "white" }}
-              />
-            </div>
-          </section>
-          <section className="flex flex-col">
+          <FirstStep
+            ingredient={ingredient}
+            handleIngredientChange={handleIngredientChange}
+            ingredientsList={ingredientsList}
+            setIngredientsList={setIngredientsList}
+          />
+          <section className="flex flex-col w-1/3">
             <h2 className="text-heading-semibold-5 text-blue-100 mb-2">
               Passo 2:
             </h2>
             <div className="flex bg-gray-100 max-w-sm py-10 px-8 max-h-72 rounded-2xl"></div>
           </section>
-          <section className="flex flex-col">
+          <section className="flex flex-col w-1/3">
             <h2 className="text-heading-semibold-5 text-blue-100 mb-2">Fim!</h2>
             <div className="flex bg-gray-100 max-w-sm py-10 px-8 max-h-72 rounded-2xl"></div>
           </section>
