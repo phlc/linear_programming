832659774617c45259a1fc58101ce3fd62f424da
tarcila414 <tarcila086@gmail.com>
2022-10-20 21:36:06 -0300

    enables cors

--- backend/__init__.py
+++ backend/__init__.py
@@ -2,16 +2,22 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from os import path
-
+from flask_cors import CORS
 
 DB_NAME = "database.db"     # Database file name
 db = SQLAlchemy()           # SQLAlchemy database object
 ma = Marshmallow()          # Marshmallow schema object
 
+CORS_ALLOW_ORIGIN="*,*"
+CORS_EXPOSE_HEADERS="*,*"
+CORS_ALLOW_HEADERS="content-type,*"
+
 
 def create_app():
     """ Creates Flask app and sets its configurations """
     app = Flask(__name__)
+    cors = CORS(app, origins=CORS_ALLOW_ORIGIN.split(","), allow_headers=CORS_ALLOW_HEADERS.split(",") , expose_headers= CORS_EXPOSE_HEADERS.split(","),   supports_credentials = True)
+    app.config['CORS_HEADERS'] = 'Content-Type'
     app.config["SECRET_KEY"] = "falsneicm382xlkd"
     app.config["SQLALCHEMY_DATABASE_URI"] =  f'sqlite:///{DB_NAME}'
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
